ee983a95720b17de6442527f0f47a5f8c9cabb9b
0936348141 <aung.net1234@hotmail.com>
2014-11-28 11:06:40 +0700

    Revert "add new web1"
    This reverts commit 86a2a24549c81cc01fbfa7ab4ee6d9fdb247e4ef.

--- Priewpan Panjaroen_files/YT3KHj2muA-.js
+++
@@ -1,5 +0,0 @@
-/*!CK:1323337315!*//*1375938001,173209645*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["8eJ2b"]); }
-
-__d("legacy:PhotoSnowliftAds",["PhotoSnowliftAds"],function(a,b,c,d){a.PhotoSnowliftAds=b('PhotoSnowliftAds');},3);
